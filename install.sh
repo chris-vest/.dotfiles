ceefaf89bc908ea6b434095e55e5d20724f5bec4
chris-vest <hotdogsandfrenchfries@gmail.com>
2019-03-02 21:13:37 +0000

    Use https for vim

--- install.sh
+++ install.sh
@@ -146,7 +146,7 @@ install_vim() {
 
 	# install .vim files
 	sudo rm -rf "${HOME}/.vim"
-	git clone --recursive git@github.com:jessfraz/.vim.git "${HOME}/.vim"
+	git clone --recursive https://github.com:jessfraz/.vim "${HOME}/.vim"
 	(
 	cd "${HOME}/.vim"
 	make install
